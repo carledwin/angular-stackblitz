fe9e0547d931afb0e0dbd50e494e7234eeb73261
carledwin <carlinstr@gmail.com>
2020-01-13 14:31:54 -0300

    Implementação test com stackblitz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import moment from 'moment';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Angular Stackblitz';
+  now = moment().fromNow();
+  calendar = moment().calendar();
 }
+
+console.log('hello')
